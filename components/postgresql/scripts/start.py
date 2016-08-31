5a064dd24ae114a6e64de27439c9e67e8f18ac05
asdf <tehasdf@gmail.com>
2016-08-31 12:40:44 +0200

    move stolon service files to pg/

---
+++ components/postgresql/scripts/start.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from os.path import join, dirname
+
+from cloudify import ctx
+
+ctx.download_resource(
+    join('components', 'utils.py'),
+    join(dirname(__file__), 'utils.py'))
+import utils  # NOQA
+
+PG_SERVICE_NAME = 'postgresql'
+
+for service_name in ['stolon-sentinel', 'stolon-keeper', 'stolon-proxy']:
+    utils.start_service(service_name)
