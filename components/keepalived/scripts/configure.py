c82170ab8c81f96332d2be622d23f0839d5aebe2
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2016-09-30 14:21:20 +0200

    keepalived: get config from consul

--- components/keepalived/scripts/configure.py
+++ components/keepalived/scripts/configure.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import json
+import urllib2
 from os.path import join, dirname
 
 from cloudify import ctx
@@ -21,3 +23,72 @@ utils.deploy_blueprint_resource(
     '/etc/keepalived/keepalived.conf',
     KEEPALIVED_SERVICE_NAME
 )
+
+
+def _parse_consul_response(data):
+    desc = {
+        'nodes': [],
+        'virtualip': None
+    }
+    for elem in data:
+        value = json.loads(elem['Value'].decode('base64'))
+        if elem['Key'].startswith('keepalived/nodes'):
+            desc['nodes'].append(value)
+        elif elem['Key'].startswith('keepalived/virtualip'):
+            desc['master'] = value
+    return desc
+
+
+def _register_node(state, priority):
+    utils.http_request(
+        'http://127.0.0.1:8500/v1/kv/keepalived/nodes/{0}'.format(
+            ctx.instance.id),
+        data=json.dumps({'priority': priority, 'initial_state': state}),
+        method='PUT'
+    )
+
+
+def _set_virtual_ip(virtualip):
+    utils.http_request(
+        'http://127.0.0.1:8500/v1/kv/keepalived/virtualip',
+        data=json.dumps(virtualip),
+        method='PUT'
+    )
+
+
+def configure_keepalived():
+    try:
+        resp = urllib2.urlopen(
+            'http://127.0.0.1:8500/v1/kv/keepalived/?recurse')
+    except urllib2.HTTPError as e:
+        if e.code == 404:
+            data = {'virtualip': None, 'nodes': []}
+        else:
+            raise
+    else:
+        data = _parse_consul_response(json.load(resp))
+
+    if not data['nodes']:
+        priority = 100
+        state = 'MASTER'
+    else:
+        priority = min(node['priority'] for node in data['nodes']) - 1
+        state = 'BACKUP'
+
+    if data['virtualip']:
+        if ctx_properties['keepalived_floating_ip'] != data['virtualip']:
+            ctx.abort_operation('Keepalived virtualip was set to {0}, '
+                                'but cluster is using {1}'.format(
+                                    ctx_properties['keepalived_floating_ip'],
+                                    data['virtualip']))
+        virtualip = data['virtualip']
+    else:
+        _set_virtual_ip(ctx_properties['keepalived_floating_ip'])
+        virtualip = ctx_properties['keepalived_floating_ip']
+
+    _register_node(state, priority)
+    ctx.instance.runtime_properties['state'] = state
+    ctx.instance.runtime_properties['virtualip'] = virtualip
+    ctx.instance.runtime_properties['priority'] = priority
+
+configure_keepalived()
